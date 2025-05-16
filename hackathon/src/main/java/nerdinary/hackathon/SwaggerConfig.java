340f2328320210b61c0d9e2a0fefe74b2fe3b54e
0sunset0 <chi6465618@naver.com>
2025-05-16 16:23:57 +0900

    chore: 스웨거 세팅

---
+++ hackathon/src/main/java/nerdinary/hackathon/SwaggerConfig.java
@@ -0,0 +1,27 @@
+package nerdinary.hackathon;
+
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class SwaggerConfig {
+	@Bean
+	public OpenAPI openAPI() {
+		return new OpenAPI()
+			.info(new Info()
+				.title("My API")
+				.description("My API description"));
+	}
+
+	@Bean
+	public GroupedOpenApi publicApi() {
+		return GroupedOpenApi.builder()
+			.group("v1")
+			.pathsToMatch("/api/**")
+			.build();
+	}
+}
