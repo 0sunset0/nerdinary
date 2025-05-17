c815af7866cfd478b99e42588d1a55cdac575f57
0sunset0 <chi6465618@naver.com>
2025-05-18 00:15:08 +0900

    docs: 스웨거 토큰 세팅

--- hackathon/src/main/java/nerdinary/hackathon/global/config/SwaggerConfig.java
+++ hackathon/src/main/java/nerdinary/hackathon/global/config/SwaggerConfig.java
@@ -1,10 +1,14 @@
 package nerdinary.hackathon.global.config;
 
+import java.util.List;
+
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.servers.Server;
+
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,28 +23,31 @@ public class SwaggerConfig {
 			.pathsToMatch("/api/**")
 			.build();
 	}
-
 	@Bean
 	public OpenAPI openAPI() {
 		return new OpenAPI()
+			.servers(List.of(
+				new Server().url("https://goodluckynews.store")
+			))
 			.info(new Info()
 				.title("Hackathon API")
 				.description("Carbon MBTI API Documentation")
 				.version("v1"))
 			.components(new Components()
 				.addSecuritySchemes("accessToken", new SecurityScheme()
-					.name("Authorization") // Swagger UI에서 보이는 이름
 					.type(SecurityScheme.Type.HTTP)
 					.scheme("bearer")
 					.bearerFormat("JWT")
+					.name("Authorization")
 				)
 				.addSecuritySchemes("refreshToken", new SecurityScheme()
-					.name("RefreshToken")
 					.type(SecurityScheme.Type.APIKEY)
 					.in(SecurityScheme.In.HEADER)
-					.bearerFormat("JWT") // 선택 사항
+					.name("RefreshToken")
+					.bearerFormat("JWT")
 				)
 			)
-			.addSecurityItem(new SecurityRequirement().addList("accessToken")); // 기본 인증 요구
+			.addSecurityItem(new SecurityRequirement().addList("accessToken"));
 	}
 }
+
