4fab465ca9c7acb4a65bddd79847b070535f8f97
0sunset0 <chi6465618@naver.com>
2025-05-17 23:44:46 +0900

    docs: 스웨거 토큰 세팅

--- hackathon/src/main/java/nerdinary/hackathon/global/config/SwaggerConfig.java
+++ hackathon/src/main/java/nerdinary/hackathon/global/config/SwaggerConfig.java
@@ -1,21 +1,16 @@
 package nerdinary.hackathon.global.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-
 @Configuration
 public class SwaggerConfig {
-	@Bean
-	public OpenAPI openAPI() {
-		return new OpenAPI()
-			.info(new Info()
-				.title("My API")
-				.description("My API description"));
-	}
 
 	@Bean
 	public GroupedOpenApi publicApi() {
@@ -23,5 +18,29 @@ public class SwaggerConfig {
 			.group("v1")
 			.pathsToMatch("/api/**")
 			.build();
+	}
+
+	@Bean
+	public OpenAPI openAPI() {
+		return new OpenAPI()
+			.info(new Info()
+				.title("Hackathon API")
+				.description("Carbon MBTI API Documentation")
+				.version("v1"))
+			.components(new Components()
+				.addSecuritySchemes("accessToken", new SecurityScheme()
+					.name("Authorization") // Swagger UI에서 보이는 이름
+					.type(SecurityScheme.Type.HTTP)
+					.scheme("bearer")
+					.bearerFormat("JWT")
+				)
+				.addSecuritySchemes("refreshToken", new SecurityScheme()
+					.name("RefreshToken")
+					.type(SecurityScheme.Type.APIKEY)
+					.in(SecurityScheme.In.HEADER)
+					.bearerFormat("JWT") // 선택 사항
+				)
+			)
+			.addSecurityItem(new SecurityRequirement().addList("accessToken")); // 기본 인증 요구
 	}
 }
